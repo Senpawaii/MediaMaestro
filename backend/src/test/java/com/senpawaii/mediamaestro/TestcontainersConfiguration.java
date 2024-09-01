568f119b441e4a86fe50a24f5cac4b4e0ece6289
Senpawaii <joao.miguel.queiros@gmail.com>
2024-09-01 12:49:47 +0100

    - initial commit

---
+++ backend/src/test/java/com/senpawaii/mediamaestro/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package com.senpawaii.mediamaestro;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
